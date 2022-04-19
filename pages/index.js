9271cc06c60818c93ba693f4e2481958a192623b
Antoine Delcourte <adelcourte@siliconsalad.com>
2022-04-19 15:24:28 +0200

    Updating graphql request

--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,6 @@ export default function Home() {
             query: gql`
                 {
                     characters(filter: { name: "${search}" }) {
-                        info {
-                            count
-                        }
                         results {
                             name,
                             status,
